94d89a72c03a4e7343f0c233ae45edd70dcb196e
Stercy Mutuku <mutukustercy93@gmail.com>
2025-06-08 17:23:32 +0300

    for loop and while loop

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+function writeCards(names, event) {
+    let newArray = [];
+
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return newArray;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; --i) {
+        console.log(i);
+    }
+}
+
+writeCards(["Mary", "Ndunge", "Mutua"], "birthday");
+countDown(7);
+
